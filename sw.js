89a0cfd34378ee8151e7ed56459a2a384d851631
ThreeDee83 <shit.happens@gmx.at>
2026-07-08 05:14:35 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "kassenraum-v35";
+const CACHE = "kassenraum-v36";
 const APP_SHELL = ["./", "./index.html", "./styles.css", "./app.js", "./xlsx-export.js", "./cloud.js", "./config.js", "./manifest.webmanifest"];
 
 self.addEventListener("install", (event) => {
